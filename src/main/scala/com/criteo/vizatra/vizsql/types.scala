c94ee9c36aaf6706646657a7fe6d49c1a7b445a8
Michael willame <m.willame@criteo.com>
2016-03-03 11:40:00 +0100

    [REFACT] Refactoring parameter types : extracted method for unifying types
    Change-Id: Idc4fe03aa5d2fab94fd95ab7252e7c32b94db33f

--- src/main/scala/com/criteo/vizatra/vizsql/types.scala
+++ src/main/scala/com/criteo/vizatra/vizsql/types.scala
@@ -1,5 +1,8 @@
 package com.criteo.vizatra.vizsql
 
+import java.text.SimpleDateFormat
+import java.sql.{Timestamp, Date}
+
 trait Type {
   val nullable: Boolean
   def withNullable(nullable: Boolean): this.type
@@ -81,4 +84,66 @@ case class RANGE(of: Type) extends Type {
     case _ => false
   }
   def show = s"range(${of.show})"
+}
+
+object Type {
+  case class MissingParameter(err: Err) extends Throwable
+
+  def convertParamList(parameters: Map[String, Any], query : Query) = query.placeholders.right.map(placeholders =>
+      parameters.map(x => (x._1 , (placeholders.typeOf(Placeholder(Some(x._1))), x._2))).collect
+      {case (x , (Some(y), z)) => (x,(y,z))}.mapValues(x => convertParam(x._1, x._2)))
+
+  def convertParam(pType : Type, value : Any) : Any = pType match {
+    case STRING(_) => value match {
+      case x :: _ => convertParam(pType, x)
+      case s: String => s
+      case x => throw new MissingParameter(ParameterError(
+        s"unexpected value $x (${x.getClass.getName}) for an SQL STRING parameter", -1
+      ))
+    }
+    case INTEGER(_) => value match {
+      case x :: _ => convertParam(pType, x)
+      case x: Int => x
+      case x: Long => x.toInt
+      case x: String => try {
+        x.toInt
+      } catch {
+        case _: Throwable => throw new MissingParameter(ParameterError(
+          s"unexpected value $x (${x.getClass.getName}) for an SQL INTEGER parameter", -1
+        ))
+      }
+      case x => throw new MissingParameter(ParameterError(
+        s"unexpected value $x (${x.getClass.getName}) for an SQL INTEGER parameter", -1
+      ))
+    }
+    case DATE(_) => value match {
+      case x :: _ => convertParam(pType, x)
+      case x: String => new Date(new SimpleDateFormat("yyyy-MM-dd").parse(x).getTime)
+      case x : java.sql.Date => x
+      case x : java.util.Date => new java.sql.Date(x.getTime)
+      case x => throw new MissingParameter(ParameterError(
+        s"unexpected value $x (${x.getClass.getName}) for an SQL DATE parameter", -1
+      ))
+    }
+    case TIMESTAMP(_) => value match {
+      case x :: _ => convertParam(pType, x)
+      case x: String => new Timestamp(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(x).getTime)
+      case x : java.sql.Timestamp => x
+      case x : java.util.Date => new Timestamp(x.getTime)
+      case x => throw new MissingParameter(ParameterError(
+        s"unexpected value $x (${x.getClass.getName}) for an SQL TIMESTAMP parameter", -1
+      ))
+    }
+    case SET(t) => value match {
+      case x: Seq[_] => x.map(x =>convertParam(t, x)).toSet
+      case x => Set(convertParam(t, x))
+    }
+    case RANGE(t) => value match {
+      case (a,b) => (convertParam(t, a) ,convertParam(t, b))
+      case a :: b :: _ => (convertParam(t, a), convertParam(t, b))
+      case x => throw new MissingParameter(ParameterError(
+        s"unexpected value $x (${x.getClass.getName}) for an SQL RANGE parameter", -1
+      ))
+    }
+  }
 }
